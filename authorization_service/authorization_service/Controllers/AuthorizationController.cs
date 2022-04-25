21473a6b871b04828a6b350b008e985d4ffab2c4
Mikhail Didur <mihasyaterminator@gmail.com>
2022-04-25 19:31:14 +0300

    Setup the project

---
+++ authorization_service/authorization_service/Controllers/AuthorizationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace authorization_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet("register")]
+        public string register([FromBody] RegistrationData parameters)
+        {
+            return parameters.Email;
+        }
+    }
+
+    public class RegistrationData
+    {
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
